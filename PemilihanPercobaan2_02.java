8e57e2985092712f1ea223162aa2a908df9f11fe
agungfradiansyah12 <agungfradiansyah01@gmail.com>
2023-09-27 13:49:17 +0700

    jobsheet 5

--- PemilihanPercobaan2_02.java
+++ PemilihanPercobaan2_02.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class PemilihanPercobaan2_02 {
     public static void main(String[] args) {
         Scanner input02 = new Scanner(System.in);
-        System.out.print("Nilai uas: ");
+        System.out.print("Nilai uas\t : ");
         float uas = input02.nextFloat();
-        System.out.print("Nilai uts: ");
+        System.out.print("Nilai uts\t : ");
         float uts = input02.nextFloat();
-        System.out.print("Nilai kuis: ");
+        System.out.print("Nilai kuis\t : ");
         float kuis = input02.nextFloat();
-        System.out.print("Nilai Tugas: ");
+        System.out.print("Nilai Tugas\t : ");
         float tugas = input02.nextFloat();
 
         float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
@@ -22,7 +22,7 @@ public class PemilihanPercobaan2_02 {
         total > 39 && total <= 50 ? " D " :
         " E ";
 
-        System.out.println("nilai akhir anda adalah " + total + " sehingga nilai huruf anda adalah " + message);
+        System.out.println("Nilai akhir anda adalah " + total + " sehingga nilai huruf anda adalah " + message);
         
     }
 }
